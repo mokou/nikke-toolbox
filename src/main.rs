c779d4ce2253e76285e41fbf8de6cf6d9f8620d1
Mokou <nikke-toolbox@mokou.io>
2023-02-21 16:41:09 -0500

    Use include_str! for about()

--- src/main.rs
+++ src/main.rs
@@ -259,41 +259,11 @@ fn nuke() -> std::io::Result<()> {
 }
 
 fn about() {
-    println!{r#"
-A helpful set of tools to manage your Nikke installation.
-
--------------------------------------------------------------------------------
-
-HelmSTAR Icon:
-Copyright (c) 2022 Irene#7777. All rights reserved.
-
--------------------------------------------------------------------------------
-
-Everything Else:
-Copyright (c) 2023 Mokou <nikke-toolbox@mokou.io>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
--------------------------------------------------------------------------------
-
-Source code available at: https://github.com/mokou/nikke-toolbox
-"#}
+    println!("{}{}{}",
+        include_str!("../LICENSES/MIT.txt"),
+        include_str!("about.in"),
+        include_str!("../LICENSES/HelmSTAR.ico.txt")
+    );
 }
 
 fn fuck_is_symlink<P: AsRef<Path>>(path: P) -> Result<bool, std::io::Error> {
